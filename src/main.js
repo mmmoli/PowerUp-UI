a06cbed7595ff42d48f246e7a2e27b024a8a81be
Michele Memoli <michele@100shapes.com>
2015-09-12 16:43:39 +0100

    Filling-up power bar

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ let powerBar = document.querySelector('#powerBar');
 let md = Rx.Observable.fromEvent(document, 'mousedown').map(true);
 let mu = Rx.Observable.fromEvent(document, 'mouseup').map(false);
 
-md.subscribe((power) => {
-    "use strict";
-    console.log(power);
-});
+let source = Rx.Observable.interval(100);
 
-powerBar.style.height = `${20}%`;
+md
+    .flatMapLatest(() => source.takeUntil(mu))
+    .subscribe((power) => {
+        "use strict";
+        powerBar.style.height = `${power * 0.4}%`;
+    });
+
+
