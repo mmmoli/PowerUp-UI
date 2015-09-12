b2b6f4587885b8bdae7045dce74abb91ea9040a5
Michele Memoli <michele@100shapes.com>
2015-09-12 19:10:17 +0100

    Minor edits

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ let powerBar = document.querySelector('#powerBar');
 let md = Rx.Observable.fromEvent(document, 'mousedown').map(true);
 let mu = Rx.Observable.fromEvent(document, 'mouseup').map(false);
 
-let source = Rx.Observable.interval(100);
+let tick = Rx.Observable.interval(100);
+
+
 
 md
-    .flatMapLatest(() => source.takeUntil(mu))
+    .flatMap(() => tick.takeUntil(mu))
     .subscribe((power) => {
         "use strict";
         powerBar.style.height = `${power * 0.4}%`;
